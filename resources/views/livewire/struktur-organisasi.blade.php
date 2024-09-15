8b0c45b6a5781352b0ea482fd3971d0d1810671d
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-09-15 22:04:49 +0700

    Add: sponsorship section

--- resources/views/livewire/struktur-organisasi.blade.php
+++ resources/views/livewire/struktur-organisasi.blade.php
@@ -5,7 +5,11 @@
     <div class="min-h-screen p-6">
         <div class="p-4 border-gray-400 border-2 flex flex-col gap-5 shadow-md">
             <h1 class="font-bold text-xl">Struktur Organisasi SMKN 1 Bantul</h1>
-            <img class="w-full object-cover" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjwWNkCTEG8rSzepqqhkaPexVLt49BNSPAi9M8WylO851xqwurYiB2w2G48jb_xCHKroCltcc3IRrCqgtcMPQRYhhbRxmVS728hKAhYuPXps-FdGz69CouhjGcKUBTJ9RJoZ1ctxT_3xA8/s1600/STRUKTUR+ORGANISASI+2011+c.JPG" alt="Struktur organisasi">
+            <div class="w-full overflow-x-scroll">
+                <img class="w-[800px] object-cover"
+                    src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjwWNkCTEG8rSzepqqhkaPexVLt49BNSPAi9M8WylO851xqwurYiB2w2G48jb_xCHKroCltcc3IRrCqgtcMPQRYhhbRxmVS728hKAhYuPXps-FdGz69CouhjGcKUBTJ9RJoZ1ctxT_3xA8/s1600/STRUKTUR+ORGANISASI+2011+c.JPG"
+                    alt="Struktur organisasi">
+            </div>
         </div>
     </div>
 </div>
