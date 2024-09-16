0546b92c3ce03a32b088540d2d7128c58818dce1
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-09-16 20:59:58 +0700

    Fix: Responsif berita detail page

--- resources/views/livewire/berita/berita-detail.blade.php
+++ resources/views/livewire/berita/berita-detail.blade.php
@@ -1,10 +1,10 @@
 <div>
-    <x-header.index slug="{{$berita->slug}}" src="{{$berita->attachment->path}}">
+    <x-header.index slug="DetailBerita" src="{{$berita->attachment->path}}">
         {{$berita->title}}
     </x-header.index>
-    <div class="w-screen min-h-screen py-10 px-10 md:px-36">
+    <div class="w-screen min-h-screen py-10 px-8 md:px-36">
         <div class="flex flex-col gap-8">
-            <div class="font-bold text-gray-600 flex justify-between">
+            <div class="font-bold text-gray-600 flex justify-between text-sm">
                 <span>Uploaded {{$berita->created_at->diffForHumans()}}</span>
                 <span>By SMKN 1 BANTUL</span>
             </div>
