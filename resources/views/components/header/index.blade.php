eda30d55faca1aee60d48b5bec48e9f47b7eec63
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-09-17 12:20:13 +0700

    Fix: berita detail page slug

--- resources/views/components/header/index.blade.php
+++ resources/views/components/header/index.blade.php
@@ -4,6 +4,6 @@
     </h1>
     <img src="{{$src??'https://upload.wikimedia.org/wikipedia/commons/b/b6/Front_view_of_SMKN_1_Bantul.jpg'}}" alt="banner" class="absolute top-0 left-0 w-screen h-full object-cover brightness-50">
     <div class="bottom-0 bg-blue-600 left-0 absolute w-full z-20">
-        <div class="text-white text-lg flex items-center gap-3 h-full px-8 py-4 bg-blue-800 w-fit"><a href="/">Home</a> <span class="text-sm">>></span><h3 class="text-white">{{$slug ?? $slot}}</h3></h>
+        <div class="text-white text-lg flex items-center gap-3 h-full px-8 py-4 bg-blue-800 w-fit"><a href="/">Home</a> <span class="text-sm">>></span><h3 class="text-white text-nowrap overflow-hidden text-ellipsis">{{$slug ?? $slot}}</h3></h>
     </div>
 </header>
--- resources/views/livewire/berita/berita-detail.blade.php
+++ resources/views/livewire/berita/berita-detail.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-header.index slug="DetailBerita" src="{{$berita->attachment->path}}">
+    <x-header.index slug="{{$berita->slug}}" src="{{$berita->attachment->path}}">
         {{$berita->title}}
     </x-header.index>
     <div class="w-screen min-h-screen py-10 px-8 md:px-36">
