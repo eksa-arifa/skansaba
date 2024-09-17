4b0ee8ab48a1fbe2f71a2c269997a987ff1d6991
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-09-17 13:38:01 +0700

    Making Dynamic: Berita in wellcome

--- app/Livewire/Welcome.php
+++ app/Livewire/Welcome.php
@@ -2,12 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Models\Berita;
 use Livewire\Component;
 
 class Welcome extends Component
 {
     public function render()
     {
-        return view('livewire.welcome')->layout('layouts.app');
+        try{
+            $berita = Berita::latest()->first();
+
+            if(! $berita){
+                throw new \Exception();
+            }
+
+            $beritas = Berita::latest()->offset(1)->limit(4)->get();
+
+            if(! $beritas){
+                throw new \Exception();
+            }
+
+            return view('livewire.welcome', ["berita"=>$berita, "beritas"=>$beritas])->layout('layouts.app');
+        }catch(\Exception $e){
+            abort($e->getCode()??500, $e->getMessage());
+        }
     }
 }
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -55,17 +55,16 @@
         <h1 class="text-3xl underline decoration-blue-600" data-aos="fade-right">Berita Terbaru</h1>
         <div class="flex flex-col md:flex-row gap-4 overflow-hidden">
             <div class="w-full md:w-1/2 h-[470px] shadow-lg my-5 rounded-lg overflow-hidden" data-aos="zoom-in-right">
-                <img class="w-full h-[350px] object-cover" src="https://smkn1bantul.wordpress.com/wp-content/uploads/2024/08/img_0137.jpg?resize=1500,1500&strip=info" alt="news">
-                <a class="p-4 block cursor-pointer hover:text-blue-600 w-full whitespace-nowrap overflow-hidden text-ellipsis text-lg">Siswa SMKN 1 Bantul tersenyum lebar bersama teman temannya hahaha lucu yaa</a>
-                <p class="text-gray-500 px-4">Di upload 20 jam yang lalu oleh SMKN 1 Bantul</p>
+                <img class="w-full h-[350px] object-cover" src="{{$berita->attachment->path}}" alt="news">
+                <a class="p-4 block cursor-pointer hover:text-blue-600 w-full whitespace-nowrap overflow-hidden text-ellipsis text-lg" href="{{route('berita.detail', ["slug"=>$berita->slug])}}">{{$berita->title}}</a>
+                <p class="text-gray-500 px-4">{{$berita->created_at->diffForHumans()}} By SMKN 1 BANTUL</p>
             </div>
             <div class="flex w-full md:w-1/2 flex-col gap-3 my-5" data-aos="zoom-in-left">
                 <h2 class="text-2xl underline decoration-blue-600">Berita lainnya</h2>
                 <ul class="w-full p-4">
-                    <li class="w-full border-b-2 py-3 flex flex-col"><a class="hover:text-blue-500 text-xl w-full text-ellipsis text-nowrap overflow-hidden" href="">Berita lainnya 1 jika kamu meras gagal menjadi manusia</a><p class="text-gray-500">Di upload 25 jam yang lalu oleh SMKN 1 Bantul</p></li>
-                    <li class="w-full border-b-2 py-3 flex flex-col"><a class="hover:text-blue-500 text-xl w-full text-ellipsis text-nowrap overflow-hidden" href="">Berita lainnya 1 jika kamu meras gagal menjadi manusia</a><p class="text-gray-500">Di upload 25 jam yang lalu oleh SMKN 1 Bantul</p></li>
-                    <li class="w-full border-b-2 py-3 flex flex-col"><a class="hover:text-blue-500 text-xl w-full text-ellipsis text-nowrap overflow-hidden" href="">Berita lainnya 1 jika kamu meras gagal menjadi manusia</a><p class="text-gray-500">Di upload 25 jam yang lalu oleh SMKN 1 Bantul</p></li>
-                    <li class="w-full border-b-2 py-3 flex flex-col"><a class="hover:text-blue-500 text-xl w-full text-ellipsis text-nowrap overflow-hidden" href="">Berita lainnya 1 jika kamu meras gagal menjadi manusia</a><p class="text-gray-500">Di upload 25 jam yang lalu oleh SMKN 1 Bantul</p></li>
+                    @foreach($beritas as $item)
+                    <li class="w-full border-b-2 py-3 flex flex-col"><a class="hover:text-blue-500 text-xl w-full text-ellipsis text-nowrap overflow-hidden" href="{{route('berita.detail', ["slug"=>$item->slug])}}">{{$item->title}}</a><p class="text-gray-500">{{$item->created_at->diffForHumans()}} By SMKN 1 BANTUL</p></li>
+                    @endforeach
                 </ul>
                 <a href="" class="py-3 px-9 cursor-pointer bg-blue-600 text-white rounded-full hover:bg-blue-700 w-fit block">Show More</a>
             </div>
