5d8567f1400e1b796116e95d90320ee18a87051e
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-09-18 10:34:01 +0700

    Rework: making simple ui for media section

--- resources/views/components/media-card/index.blade.php
+++ resources/views/components/media-card/index.blade.php
@@ -1,6 +1,6 @@
-<div class="w-[170px] h-[170px] bg-white rounded-md shadow-xl" data-aos="fade-up">
+<div class="md:w-[170px] md:h-[170px] w-[120px] h-[120px] bg-white rounded-md shadow-xl" data-aos="fade-up">
     <a href="{{$href}}" class="w-full h-full p-6 flex flex-col justify-center items-center">
-        <span class="text-8xl">
+        <span class="text-4xl md:text-8xl">
             <i class="fa-brands fa-{{$logo}}"></i>
         </span>
         <span class="text-xl">{{$slot}}</span>
