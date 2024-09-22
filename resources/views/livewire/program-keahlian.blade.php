d58dc317e8fb56dbd4316c4d7d6d93665afc70d6
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-09-22 15:28:01 +0700

    Revisi: program keahlian page

--- resources/views/livewire/program-keahlian.blade.php
+++ resources/views/livewire/program-keahlian.blade.php
@@ -6,16 +6,16 @@ Program Keahlian
         Program Keahlian
     </x-header.index>
     <div class="w-screen min-h-screen p-8">
-        <div class="flex flex-wrap items-center justify-center gap-6 w-full">
+        <div class="flex flex-wrap items-center gap-6 w-full">
             <img class="w-[350px] h-[400px] object-cover" src="{{$major->image ?? ''}}" alt="Jurusan">
-            <div class="flex flex-col gap-5 h-[400px] w-[350px]">
+            <div class="flex flex-col gap-5 min-h-[400px] w-auto">
                 <h1 class="uppercase text-xl font-bold border-b-2 border-gray-400">Program Keahlian {{$major->name}}</h1>
                 <p>
                     {{$major->description}}
                 </p>
             </div>
         </div>
-        <div class="w-full mt-3">
+        <div class="w-full mt-6">
             <h1 class="uppercase text-xl font-bold">Materi Yang Diajarkan</h1>
             <div class="w-full mt-3 flex flex-wrap gap-2">
                 @forelse($major->lessons as $item)
