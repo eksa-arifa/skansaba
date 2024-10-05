aca9cce9f5d69960024299ce9a5467339a666a86
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-10-05 13:25:12 +0700

    Remake: Social media logo

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -122,18 +122,10 @@ Wassalamualaikum warahmatullahi wabarakatuh.
 <h1 class="text-center text-3xl underline decoration-blue-600">Media</h1>
 
 <div class="flex gap-4 py-10 justify-center items-center flex-wrap">
-    <x-media-card.index href="https://www.youtube.com/@officialsmkn1bantul" logo="youtube">
-        YouTube
-    </x-media-card.index>
-    <x-media-card.index href="https://instagram.com/smkn1bantul" logo="instagram">
-        Instagram
-    </x-media-card.index>
-    <x-media-card.index href="https://x.com/skansaba_id" logo="twitter">
-        Twitter
-    </x-media-card.index>
-    <x-media-card.index href="https://tiktok.com/@skansaba.id" logo="tiktok">
-        TikTok
-    </x-media-card.index>
+    <x-media-card.index href="https://www.youtube.com/@officialsmkn1bantul" logo="youtube"/>
+    <x-media-card.index href="https://instagram.com/smkn1bantul" logo="instagram"/>
+    <x-media-card.index href="https://x.com/skansaba_id" logo="twitter"/>
+    <x-media-card.index href="https://tiktok.com/@skansaba.id" logo="tiktok"/>
 </div>
 
 </div>
