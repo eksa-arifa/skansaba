62e9cc6cb90111a2c1bdd54b23e5cdca2555d083
Eksa Arifa <arifamuhammad1536@gmail.com>
2024-10-13 07:07:52 +0700

    Fix annoying error file upload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'no-auth' => NoAuth::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
